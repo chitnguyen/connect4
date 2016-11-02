d293bba6f8a950952828c3ebb96dc47d824c28ed
Nguyen Thuy Chi <hongtuki@yahoo.com>
2016-11-02 20:43:14 +0000

    attempt to add socket

--- connect4/views.py
+++ connect4/views.py
@@ -31,7 +31,7 @@ def login(request):
             user = authenticate(username=username, password=password)
             if user is not None:
                 auth.login(request, user)
-                return redirect(games)
+                return HttpResponseRedirect('/connect4/games/')
     else:
         form = AuthenticationForm()
 
