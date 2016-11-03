85b368004422230a9b21b23c1b38ce94abd1c98a
Nguyen Thuy Chi <hongtuki@yahoo.com>
2016-11-03 01:26:00 +0000

    attempt to add socket

--- connect4/views.py
+++ connect4/views.py
@@ -30,7 +30,7 @@ def login(request):
             user = authenticate(username=username, password=password)
             if user is not None:
                 auth.login(request, user)
-                #return redirect(games)
+                # return redirect(games)
                 return HttpResponseRedirect('/connect4/games/')
     else:
         form = AuthenticationForm()
@@ -71,7 +71,7 @@ def signup(request):
     return render_to_response('signup.html', args)
 
 
-# @login_required
+@login_required(login_url='/connect4/login/')
 def games(request):
     """
     Write your view which controls the game set up and selection screen here
@@ -98,7 +98,7 @@ def games(request):
     return render(request, 'game.html', context)
 
 
-# @login_required
+@login_required(login_url='/connect4/login/')
 def play(request, game_id):
     """
     write your view which controls the gameplay interaction w the web layer here
