ed6f00c182dde2c0c61aff67958581547a038293
Nguyen Thuy Chi <hongtuki@yahoo.com>
2016-11-03 01:46:54 +0000

    attempt to add socket

--- connect4/views.py
+++ connect4/views.py
@@ -70,7 +70,6 @@ def signup(request):
     return render_to_response('signup.html', args)
 
 
-# @login_required(login_url='/connect4/login/')
 def games(request):
     """
     Write your view which controls the game set up and selection screen here
@@ -99,28 +98,30 @@ def games(request):
         return render(request, 'game.html', context)
 
 
-# @login_required(login_url='/connect4/login/')
 def play(request, game_id):
     """
     write your view which controls the gameplay interaction w the web layer here
     :param request, game_id:
     :return:
     """
-    game = Game.objects.get(id=game_id)
-    if game.player1 != request.user:
-        game.join_up(request.user)
+    if not request.user.is_authenticated():
+        return HttpResponseRedirect('/connect4/login/')
+    else:
+        game = Game.objects.get(id=game_id)
+        if game.player1 != request.user:
+            game.join_up(request.user)
 
-    coin_set = game.coin_set
-    player_turns = [coin.player_id for coin in game.coin_set.all()]
-    rows = [coin.row for coin in game.coin_set.all()]
-    cols = [coin.column for coin in game.coin_set.all()]
-    last_move = game.last_move if len(game.coin_set.all()) else None
-    context = {
-        'game': game,
-        'coin_set': coin_set,
-        'player_turns': player_turns,
-        'rows': rows,
-        'cols': cols,
-        'last_move': last_move
-    }
-    return render(request, 'play.html', context)
+        coin_set = game.coin_set
+        player_turns = [coin.player_id for coin in game.coin_set.all()]
+        rows = [coin.row for coin in game.coin_set.all()]
+        cols = [coin.column for coin in game.coin_set.all()]
+        last_move = game.last_move if len(game.coin_set.all()) else None
+        context = {
+            'game': game,
+            'coin_set': coin_set,
+            'player_turns': player_turns,
+            'rows': rows,
+            'cols': cols,
+            'last_move': last_move
+        }
+        return render(request, 'play.html', context)
